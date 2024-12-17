7a6fcc5ec458401cd0673a3f3d4394e91d01510c
kevinlee-06 <cgshyuankai@proton.me>
2024-12-17 09:30:36 +0800

    Fix: Import font directly from github.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
 FROM python:3.9-slim
 RUN apt update
-RUN apt install -y pandoc tree
+RUN apt install -y pandoc
 RUN pip install Flask
 WORKDIR /var/www/html
 COPY docs .
-COPY assets ./assets
 COPY build .
 RUN pandoc aoa.md -o index.html
 EXPOSE 5000
-RUN tree .
 CMD ["python", "app.py"]
--- build/app.py
+++ build/app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def serve_index():
     return send_from_directory('.', 'index.html')
 
-@app.route('/assets/<path:filename>')
-def serve_assets(filename):
-    return send_from_directory('assets', filename)
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
